d382bd7fe3cefa6cafdf139696e5a22cc26c5b01
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2018-11-02 13:49:46 +0800

    开发、生产环境配置分离

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,21 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack')
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'web2019',
-      filename: 'index.html',
-      hash: true
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+      title: 'web2019'
+    })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development',
   module: {
     rules: [
       {
