56004571cc4840d30522e362750ca17f3cc1f2f8
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2018-12-20 09:56:08 +0800

    配置头部占位

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  // .header + div {
-  //   padding-top: 30px;
-  // }
+  .header + div {
+    padding-top: 30px;
+  }
 </style>
--- src/component/Header/index.vue
+++ src/component/Header/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <div class="container">
+      <div class="fz-middle">头部</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +13,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .header {
+    position: fixed;
+    width: 100%;
+    background-color: #fff;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+  }
 </style>
