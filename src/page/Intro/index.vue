83eae4144d6a5f19a7881cef00b6895e12f84f89
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2018-12-21 13:38:26 +0800

    开发 intro 页面

--- src/page/Intro/index.vue
+++ src/page/Intro/index.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="intro container">
-    <div>hello intro!</div>
-    <router-link to="/article/1">朱自清-背影-demo</router-link>
+    <div class="lrc" :style="{ height: `${height}px` }">
+      <router-link
+        to="/recommend"
+        class="fz-normal"
+        :style="{ 'margin-top': `${ 1/3 * height }px` }"
+      >
+        假如你不曾让我悲伤<br />我怎会感觉昏暗无光
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'intro'
+  name: 'intro',
+  data: () => ({
+    height: 0
+  }),
+  methods: {
+    calcHeight () {
+      this.height = document.documentElement.clientHeight - 50
+    }
+  },
+  mounted () {
+    this.calcHeight()
+    window.addEventListener('resize', this.calcHeight)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .lrc {
+    text-align: center;
+  }
+  .lrc a {
+    display: inline-block;
+    color: #FF6A6A;
+  }
 </style>
