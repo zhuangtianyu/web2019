41db150aa96c1681a9675b538551f14ef167afcc
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-01-28 23:28:27 +0800

    配置文件名 chunkhash, 避免缓存

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
     new VueLoaderPlugin()
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/'
   },
