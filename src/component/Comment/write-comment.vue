307bb8ddfa38170f24699a7a7193ff4259376f5e
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-03-25 10:38:07 +0800

    将评论的长度限制扩展到2000字

--- src/component/Comment/write-comment.vue
+++ src/component/Comment/write-comment.vue
@@ -48,8 +48,8 @@ export default {
       if (this.params.content === '') {
         return this.$modal({ message: '请输入评论' })
       }
-      if (this.params.content.length > 600) {
-        return this.$modal({ message: '评论长度不超过600字' })
+      if (this.params.content.length > 2000) {
+        return this.$modal({ message: '评论长度不超过2000字' })
       }
       return true
     },
@@ -60,7 +60,7 @@ export default {
       if (this.validate() !== true) { return }
       const articleID = this.$route.params.id
       const { userName, content } = this.params
-      this.$axios.post('/article/comment', { userName, content: content.replace(/\n/g,'<br />'), articleID })
+      this.$axios.post('/article/comment', { userName, content: content.replace(/\n/g, '<br />'), articleID })
         .then(() => {
           this.$emit('reload')
           this.cancel()
@@ -71,71 +71,71 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .write-comment {
-    padding: 6px 0 20px;
-  }
+.write-comment {
+  padding: 6px 0 20px;
+}
 
-  .write-item  {
-    margin-bottom: 12px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+.write-item {
+  margin-bottom: 12px;
+  &:last-child {
+    margin-bottom: 0;
   }
+}
 
-  .write-item {
-    font-size: 0;
-    .label {
-      display: inline-block;
-      vertical-align: middle;
-      width: 40px;
-      font-size: 14px;
-    }
-    .write-item-content {
-      display: inline-block;
-      vertical-align: middle;
-      width: 100%;
-      padding-left: 40px;
-      font-size: 14px;
-      margin-left: -40px;
-    }
-    &.content {
-      padding-bottom: 6px;
-      border-bottom: 1px solid #fff;
-      .label,
-      .write-item-content {
-        vertical-align: top;
-      }
-    }
-  }
-
-  .write-item-content input {
-    width: 60%;
-    max-width: 230px;
+.write-item {
+  font-size: 0;
+  .label {
+    display: inline-block;
+    vertical-align: middle;
+    width: 40px;
     font-size: 14px;
-    height: 24px;
-    line-height: 20px;
-    padding: 0 3px;
   }
-
-  .write-item-content textarea {
-    display: block;
-    width: 90%;
-    height: 60px;
-    font-size: 14px;
-    line-height: 20px;
-    padding: 0 3px;
-    margin-bottom: 10px;
-    resize: none;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-
-  .button-group {
+  .write-item-content {
+    display: inline-block;
+    vertical-align: middle;
+    width: 100%;
     padding-left: 40px;
-    font-size: 0;
-    .submit {
-      margin-right: 10px;
+    font-size: 14px;
+    margin-left: -40px;
+  }
+  &.content {
+    padding-bottom: 6px;
+    border-bottom: 1px solid #fff;
+    .label,
+    .write-item-content {
+      vertical-align: top;
     }
   }
+}
+
+.write-item-content input {
+  width: 60%;
+  max-width: 230px;
+  font-size: 14px;
+  height: 24px;
+  line-height: 20px;
+  padding: 0 3px;
+}
+
+.write-item-content textarea {
+  display: block;
+  width: 90%;
+  height: 60px;
+  font-size: 14px;
+  line-height: 20px;
+  padding: 0 3px;
+  margin-bottom: 10px;
+  resize: none;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.button-group {
+  padding-left: 40px;
+  font-size: 0;
+  .submit {
+    margin-right: 10px;
+  }
+}
 </style>
