5e94c70f9364565524a41b98f9423158ace7e316
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-04-10 10:46:35 +0800

    优化 resume 页面样式

--- src/page/Resume/index.vue
+++ src/page/Resume/index.vue
@@ -2,33 +2,33 @@
   <div class="resume container fz-normal">
     <div class="base-information resume-item">
       <div class="base-item">
-        <span>庄天宇</span>
-        <span class="fz-small">前端开发工程师</span>
+        <span class="fz-large">庄天宇</span>
+        <span>前端开发工程师</span>
       </div>
       <div class="base-item fz-small">热爱原生javascript, 热爱vue.js, node.js</div>
     </div>
     <div class="contact-information resume-item">
       <div class="contact-item">
-        <span class="fz-small">邮箱: </span>
+        <span>邮箱: </span>
         <span>zhuangtianyu1995@163.com</span>
       </div>
       <div class="contact-item">
-        <span class="fz-small">github: </span>
+        <span>github: </span>
         <a href="https://github.com/zhuangtianyu" target="_blank">我的github</a>
       </div>
     </div>
     <div class="project-remote resume-item">
-      <div class="project-item fz-small">本项目相关地址:</div>
+      <div class="project-item">本项目相关地址:</div>
       <div class="project-item">
-        <span class="fz-small">前台: </span>
+        <span>前台: </span>
         <a href="https://github.com/zhuangTianYu/web2019" target="_blank">web2019</a>
       </div>
       <div class="project-item">
-        <span class="fz-small">后台: </span>
+        <span>后台: </span>
         <a href="https://github.com/zhuangTianYu/web2019-backend" target="_blank">web2019-backend</a>
       </div>
       <div class="project-item">
-        <span class="fz-small">SQL: </span>
+        <span>SQL: </span>
         <a href="https://github.com/zhuangTianYu/web2019-sql" target="_blank">web2019-sql</a>
       </div>
     </div>
